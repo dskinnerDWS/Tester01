b29d2e271b2e9a16419f0d96abbd9bf03adc9373
Daniel S <dskinnerDWS@outlook.com>
2018-08-23 11:45:44 +1000

    Removed Linq namespace

--- DanController.cs
+++ DanController.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +11,7 @@ namespace WebApplication1.Controllers
         // GET: Dan
         public ActionResult Index()
         {
+// REMOVE LINQ
             return View();
         }
     }
